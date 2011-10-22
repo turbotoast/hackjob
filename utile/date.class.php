eac6f7f9b10dd733ea9663ea66a904fdc0a8063d
Jörn Meyer <mail@joern-meyer.net>
2011-10-22 23:17:15 +0200

    Added date utile

---
+++ utile/date.class.php
@@ -0,0 +1,56 @@
+<?php
+
+class HackJob_Utile_Date
+{
+	public $timestamp;
+	
+	public function __construct($param = null)
+	{
+		if(is_int($param))
+		{
+			return self::fromTimestamp($param);
+		}
+		elseif(is_string($param))
+		{
+			return self::fromString($param);
+		}
+	}
+	
+	public static function fromTimestamp($timestamp)
+	{
+		$instance = new self();
+		$instance->timestamp = $timestamp;
+		return $instance;
+	}
+	
+	public static function fromNow()
+	{
+		return self::fromTimestamp(time());
+	}
+	
+	public static function fromString($string)
+	{
+		$instance = new self();
+		$instance->timestamp = strtotime($string);
+		if(!$instance->timestamp)
+		{
+			throw new HackJob_Utile_DateException(sprintf(
+				'Could not parse date string: %s', $string
+			));
+		}
+		return $instance;
+	}
+	
+	public function toGermanDate($withTime = false)
+	{
+		$format = $withTime ? 'd.m.Y H:i:s' : 'd.m.Y';
+		return $this->toFormat($format);
+	}
+	
+	public function toFormat($dateFormat)
+	{
+		return date($dateFormat, $this->timestamp);
+	}
+}
+
+?>
---
+++ utile/dateexception.class.php
@@ -0,0 +1,6 @@
+<?php
+
+class HackJob_Utile_DateException
+	extends HackJob_Core_Exception {}
+
+?>
