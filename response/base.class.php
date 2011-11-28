9cbeacb9e9d7b1c4c4e324d18a7ca2757d1053c8
Jörn Meyer <mail@joern-meyer.net>
2011-11-28 19:39:03 +0100

    Fixed a bug that prevented the content type to be set outside of the response class.

--- response/base.class.php
+++ response/base.class.php
@@ -12,9 +12,20 @@ abstract class HackJob_Response_Base
 		$this->headers = $headers;
 		$this->status = $status;
 		
-		$contentHeader = 'Content-type: ' . HackJob_Conf_Provider::get('DEFAULT_CONTENT_TYPE', 'text/html');
-		$contentHeader .= '; charset=' . HackJob_Conf_Provider::get('DEFAULT_CONTENT_CHARSET', 'utf8');
-		$this->headers[] = $contentHeader;
+		$contentTypeSet = false;
+		foreach($this->headers as $header)
+		{
+			if(strpos(strtolower($header), 'content-type') !== FALSE)
+			{			
+				$contentTypeSet = true;
+			}
+		}
+		if(!$contentTypeSet)
+		{
+			$contentHeader = 'Content-type: ' . HackJob_Conf_Provider::get('DEFAULT_CONTENT_TYPE', 'text/html');
+			$contentHeader .= '; charset=' . HackJob_Conf_Provider::get('DEFAULT_CONTENT_CHARSET', 'utf8');
+			$this->headers[] = $contentHeader;
+		}
 	}
 }
 
